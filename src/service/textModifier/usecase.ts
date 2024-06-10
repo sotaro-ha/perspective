d1fe4e74abe510260b2a6f04cc57c1b8ea79e797
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-06-10 23:29:55 +0900

    set send usecase type

--- src/service/textModifier/usecase.ts
+++ src/service/textModifier/usecase.ts
@@ -1,9 +1,17 @@
 import { modifyText } from "@/generated/api";
 import { ModifyTextBody } from "@/generated/model";
 
-import { UsecaseResultError, UsecaseResultOk } from "@/utils/result";
+import { UsecaseResultError, UsecaseResultOk, UsecaseMethod } from "@/utils/result";
 
-export const sendTextToAI = async (text: string, index: number, id: number) => {
+export const sendTextToAI = (async ({
+    text,
+    index,
+    id,
+}: {
+    text: string;
+    index: number;
+    id: number;
+}) => {
     const reqBody: ModifyTextBody = {
         targetText: text,
         textIndex: index,
@@ -16,4 +24,4 @@ export const sendTextToAI = async (text: string, index: number, id: number) => {
     } catch (error) {
         return UsecaseResultError(new Error("AI送信失敗"));
     }
-};
+}) satisfies UsecaseMethod;
