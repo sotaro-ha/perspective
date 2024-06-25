8b7ee3d862afa1925d0b94234b0532379ef3ed32
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-06-25 17:05:54 +0900

    update base url

--- src/states/socket.tsx
+++ src/states/socket.tsx
@@ -1,7 +1,7 @@
 import { atom, useAtom } from "jotai";
 import io, { Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.SOCKET_URL ?? "http://localhost:8081";
+const SOCKET_URL = process.env.SOCKET_URL ?? "http://192.168.0.105:8081";
 const defaultSocket = io(SOCKET_URL);
 const socketAtom = atom(defaultSocket);
 
