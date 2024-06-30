f09bdf25d51198f2d5033e7eee75bab2d3ea43cf
Haruma HIRABAYASHI <82667808+hrm1810884@users.noreply.github.com>
2024-06-30 21:40:05 +0900

    #53/modal (#55)
    * add component dir
    
    * add experience states
    
    * add vanilla styling
    
    * add modal state
    
    * update styling

--- src/components/page/StreamerPage/components/EndModal/EndModal.css.ts
+++ src/components/page/StreamerPage/components/EndModal/EndModal.css.ts
@@ -1,6 +1,14 @@
 import { style } from "@vanilla-extract/css";
 
 export const contentContainer = style({
-    justifyContent: "flex-end",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
     marginTop: "1rem",
+    gap: "1rem 1rem",
+});
+
+export const buttonStyle = style({
+    display: "flex",
+    width: "100%",
 });
